e56be0b51708a3a5ba2157f71eb6d519acca3bad
JosePimienta <pimientavalientejoseluis@gamil.com>
2023-10-12 22:35:05 -0500

    agregando los productos para bats y glove

--- js/script.js
+++ js/script.js
@@ -8,22 +8,32 @@
 // Obtener los elementos por su id
 
 window.onload=function(){
+
   var m_select = document.getElementById("m_select");
- var sel = document.getElementById("sel");
+  var sel = document.getElementById("sel");
+  // var m_sel = document.getElementById("sel");
+  
+   // iniciar el valor del display siempre en none
+  var display = 0;
  
- // Definir una función que cambie el display del ul
+  // Definir una función que cambie el display del ul
  function toggleSel() {
-   // Obtener el valor actual del display
-   var display = sel.style.display;
+ 
    // Cambiar el valor según el caso
-   if (display == "none") {
-      sel.style.display = "block";
-     sel.style.transition = 'all 0.1s ease-out 0.1s'
+   if (display == 0) {
+    sel.style.display = "block";
+    sel.style.transition = 'all 0.1s ease-out 0.1s'
+    m_select.classList.toggle('m');
+    display = 1;
    } else {
      sel.style.display = "none";
      sel.style.transition = 'all 0.1s ease-out 0.1s'
+     m_select.classList.toggle('m');
+     display = 0;
+
    }
  }
  
  // Agregar un listener al evento de clic del div
- m_select.addEventListener("click", toggleSel);}
+ m_select.addEventListener("click", toggleSel);
+}
